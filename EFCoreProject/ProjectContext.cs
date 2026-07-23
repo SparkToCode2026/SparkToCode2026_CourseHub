1d7eaf4858600bade8abd121a59f3a01693dd39d
KarimSalaheldeen <eng.karimsalaheldeen@gmail.com>
2026-07-23 09:49:13 +0400

    add efcore project

---
+++ EFCoreProject/ProjectContext.cs
@@ -0,0 +1,23 @@
+﻿using EFCoreProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreProject
+{
+    public class ProjectContext : DbContext //oop inhertance
+    {
+        //1- register models
+       public DbSet<Employee> employees {  get; set; }
+       public DbSet<Department> departments { get; set; }
+
+        //2- connect to database
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(
+            "Server=.;Database=CompanyProjectDB;Trusted_Connection=True;TrustServerCertificate=True;"
+            );
+        }
+    }
+}
